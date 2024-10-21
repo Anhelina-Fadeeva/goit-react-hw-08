12f1c325adde20c1d2889c86075ac016ad1f0784
Anhelina <fadeevalina20@gmail.com>
2024-10-21 17:50:25 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import PrivateRoute from "./components/PrivateRoute/PrivateRoute.jsx";
 
 // Ліниве завантаження сторінок
 const WelcomePage = lazy(() => import("./pages/WelcomePage"));
-const Register = lazy(() => import("./pages/RegistrationPage/RegistrationPage.jsx"));
+const Register = lazy(() => import("./pages/RegistrationPage/RegistrationPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage.jsx"));
 const Contacts = lazy(() => import("./pages/ContactsPage.jsx"));
 
